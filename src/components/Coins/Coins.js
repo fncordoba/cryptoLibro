bd83e7489ffb950dc5110a6b2e52a19c9a27e981
facundo cordoba <facucor.15@gmail.com>
2021-02-05 00:19:52 -0300

    crypto book: first commit

---
+++ src/components/Coins/Coins.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './Coins.css';
+
+class Coins extends Component {
+  constructor() {
+    super();
+
+    // Estado inicial...
+    this.state = {
+      dollars: 0
+    };
+  }
+
+  shouldComponentUpdate(props, state) {
+    return state.dollars % 10 === 0;
+  }
+
+  handleOnChange = e => {
+    this.setState({
+      dollars: Number(e.target.value || 0)
+    });
+  }
+
+  render() {
+    return (
+      <div className="Coins">
+        <h1>Buy Crypto Coins!</h1>
+
+        <div className="question">
+          <p>How much dollars do you have?</p>
+
+          <p><input placeholder="0" onChange={this.handleOnChange} type="text" /></p>
+        </div>
+
+        <div className="answer">
+          <p>Crypto Coin Price: $10</p>
+
+          <p>You can buy <strong>{this.state.dollars / 
+		  10}</strong> monedas.</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Coins;
